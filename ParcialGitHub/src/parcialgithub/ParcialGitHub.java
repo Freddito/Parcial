9cdb6693002883634e057b8326eab41928b10142
Usuario <Usuario@DESKTOP-T1EGAER>
2019-10-10 10:28:04 -0500

    TERMINANDO SWICH Y PARCIAL

--- ParcialGitHub/src/parcialgithub/ParcialGitHub.java
+++ ParcialGitHub/src/parcialgithub/ParcialGitHub.java
@@ -17,30 +17,38 @@ public class ParcialGitHub {
                                            "4. QUE ES UNA CLASE?\n"+
                                            "5. QUE ES UN OBJETO?\n"+
                                            "6. NOMBRE\n"+
-                                           "7. FECHA?\n"+
+                                           "7. FECHA\n"+
                                            "8. SALIR"));
         
         switch(opc){
             case 1:
-                JOptionPane.showMessageDialog(null,"Ingrese un valor numerico");
+                obj.commit();
                 break;
                 
             case 2:
+                obj.push();
                 break;
                 
             case 3:
+                obj.pull();
                 break;
                 
             case 4:
+                obj.clase();
                 break;
                 
             case 5:
+                obj.objeto();
                 break;
                 
             case 6:
+                obj.setNombre("Freddy Diaz Muñoz");
+                JOptionPane.showMessageDialog(null, obj.getNombre());
                 break;
                 
             case 7:
+                obj.setFecha("Octubre 10 , 2019.");
+                JOptionPane.showMessageDialog(null, obj.getFecha());
                 break;
                 
             case 8:
